f725410e283598df7bbf5a25d754e1a04af3877e
rabbilable <rabbilyasar@gmail.com>
2019-08-22 15:07:14 +0600

    finish api

--- app/Http/Resources/v1/ArticleResource.php
+++ app/Http/Resources/v1/ArticleResource.php
@@ -14,6 +14,18 @@ class ArticleResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'body' => $this->body
+        ];
+    }
+
+    public function with($request){
+
+        return [
+            'version' => '1.0.0',
+        ];
     }
 }
